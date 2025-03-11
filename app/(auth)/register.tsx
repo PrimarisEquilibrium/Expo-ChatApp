627b3221df5200a74e7c2b3847d3dc382e307bfb
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-03-11 13:26:36 -0400

    feat: connect appwrite storage for profile pictures, and use firestore for user profiles

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -8,13 +8,18 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  Image,
 } from "react-native";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/FirebaseConfig";
+import { auth, db } from "@/FirebaseConfig";
 import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 import DatePicker from "@/components/DatePicker";
 import FormInput from "@/components/FormInput";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { Client, ID, Storage } from "react-native-appwrite";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
 
 type Inputs = {
   username: string;
@@ -23,8 +28,36 @@ type Inputs = {
 };
 
 export default function Register() {
-  // Datepicker state
-  const [date, setDate] = useState(new Date());
+  // Date Picker state
+  const [date, setDate] = useState<Date>(new Date());
+
+  // Image Picker state
+  const [file, setFile] = useState<ImagePicker.ImagePickerAsset | null>(null);
+
+  const projectId = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID;
+  const endpoint = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT;
+  const bucketId = process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID;
+
+  if (!projectId || !endpoint || !bucketId) {
+    throw new Error("Appwrite project ID, endpoint, or bucket ID is missing!");
+  }
+
+  const client = new Client().setEndpoint(endpoint).setProject(projectId);
+
+  const storage = new Storage(client);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
 
   const {
     control,
@@ -40,14 +73,54 @@ export default function Register() {
 
   const onSubmit = async (data: Inputs) => {
     try {
-      const user = await createUserWithEmailAndPassword(
+      const { user: user } = await createUserWithEmailAndPassword(
         auth,
         data.email,
         data.password
       );
+
+      let profilePictureUrl = null;
+
+      if (file) {
+        const fileInfo = await FileSystem.getInfoAsync(file.uri);
+
+        let fileSize = file.width * file.height;
+        if (fileInfo.exists) {
+          fileSize = fileInfo.size;
+        }
+        const fileToUpload = {
+          uri: file.uri,
+          type: file.type || "image/jpeg",
+          name: file.uri.split("/").pop() || "image.jpg",
+          size: fileSize,
+        };
+
+        const fileId = ID.unique();
+
+        await storage.createFile(bucketId, fileId, fileToUpload);
+
+        const fileViewUrl = storage.getFileView(bucketId, fileId);
+
+        console.log(fileViewUrl);
+
+        profilePictureUrl = fileViewUrl.toString();
+      }
+
+      const profileRef = doc(db, "profiles", user.uid);
+      try {
+        await setDoc(profileRef, {
+          username: data.username,
+          profilePictureUrl: profilePictureUrl,
+          dateOfBirth: Timestamp.fromDate(date),
+        });
+        console.log("Profile created/updated successfully!");
+      } catch (error) {
+        console.error("Error creating/updating profile:", error);
+      }
+
       if (user) router.replace("/");
     } catch (error: any) {
-      console.log("error");
+      console.log(error);
       alert("Sign up failed: " + error.message);
     }
   };
@@ -78,6 +151,25 @@ export default function Register() {
         className="mt-32"
       >
         <SafeAreaView className="p-5 mx-4">
+          <View className="flex items-center justify-center mb-5">
+            <TouchableOpacity
+              onPress={pickImage}
+              className="relative w-[120px] h-[120px] rounded-full bg-[#1E1E1E] overflow-hidden"
+            >
+              {file?.uri ? (
+                <Image
+                  source={{ uri: file.uri }}
+                  className="w-full h-full object-cover"
+                />
+              ) : (
+                <Ionicons name="person-circle" size={120} color="#BB86FC" />
+              )}
+            </TouchableOpacity>
+            <Text className="text-center text-[#BB86FC] mt-3">
+              Tap to change your avatar
+            </Text>
+          </View>
+
           <FormInput
             label="Username:"
             name="username"
@@ -108,6 +200,7 @@ export default function Register() {
             isPassword={true}
           />
 
+          {/* Submit Button */}
           <TouchableOpacity
             onPress={handleSubmit(onSubmit)}
             activeOpacity={0.7}
